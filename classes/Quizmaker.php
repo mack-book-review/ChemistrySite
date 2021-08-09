6c342ed2cc441f7f998bc60d4df52a3b87b12733
mac-book-review <alexmack235711@protonmail.com>
2021-08-09 14:33:08 -0600

    Another commit

--- classes/Quizmaker.php
+++ classes/Quizmaker.php
@@ -17,8 +17,8 @@ include "QuestionTemplate.php";
 
 class Quizmaker {
 
-	public static function Make_Sample_Quiz1() {
-		return new Quizmaker([
+	public static function Get_Sample_Questions() {
+		return [
 			new ThermoQuestion1(new Alkane(4)),
 			new ThermoQuestion2(new Alkane(2)),
 			new ThermoQuestion3(new Alkane(3)),
@@ -34,8 +34,9 @@ class Quizmaker {
 				new Solution(25.00, 0.100, "HCl"),
 				new Solution(30.000, 0.5, "NaOH"), -103.4),
 
-		]);
+		];
 	}
+
 	//Array of QuizQuestions (i.e. ThermoQuestion1, ThermoQuestion2, ThermoQuestion3, etc.)
 	private array $quiz_questions;
 
@@ -48,12 +49,75 @@ class Quizmaker {
 	}
 
 	public function get_quiz_html_form($action_page) {
+		return Quizmaker::Get_Dynamic_Quiz_HTML_Form($this->quiz_questions, $action_page);
+	}
+
+	public static function Get_DB_Quiz_Form($questions, $action_page) {
+
+		$html = "<form method='post' action='" . $action_page . "'>";
+
+		foreach ($questions as $question) {
+			$html .= Quizmaker::Get_DB_Quiz_Question_HTML($question);
+		}
+
+		$html .= "<input type='submit'>";
+		$html .= "</form>";
+
+		return $html;
+	}
+
+	public static function Get_DB_Quiz_Question_HTML($question) {
+		$id = $question['question_number'];
+		$topic = $question['topic'];
+		$text = $question['question_text'];
+		$answer = $question['correct_answer'];
+
+		$html = "<p><b>(" . $id . ")</b> " . $text . "<p>";
+
+		$html .= "<select name='question" . $topic . $id . "' id='question" . $topic . $id . "'>";
+
+		$choice_A = $question['choice_A'];
+		$choice_B = $question['choice_B'];
+		$choice_C = $question['choice_C'];
+		$choice_D = $question['choice_D'];
+
+		$html .= "<option value='A'>";
+		$html .= $choice_A;
+		$html .= "</option>";
+
+		$html .= "<option value='B'>";
+		$html .= $choice_B;
+		$html .= "</option>";
+
+		$html .= "<option value='C'>";
+		$html .= $choice_C;
+		$html .= "</option>";
+
+		$html .= "<option value='D'>";
+		$html .= $choice_D;
+		$html .= "</option>";
+
+		if (isset($question['choice_E'])) {
+			$choice_E = $question['choice_E'];
+			$html .= "<option value='E'>";
+			$html .= $choice_E;
+			$html .= "</option>";
+		}
+
+		$html .= "</select>";
+
+		$html .= "<input type='hidden' name='answer" . $id . "' value=" . $answer . ">";
+
+		return $html;
+	}
+
+	public static function Get_Dynamic_Quiz_HTML_Form($quiz_questions, $action_page) {
 
 		$html = "<form method='post' action='" . $action_page . "'>";
 
 		$i = 1;
 
-		foreach ($this->quiz_questions as $quiz_question) {
+		foreach ($quiz_questions as $quiz_question) {
 			$question_template = new QuestionTemplate($quiz_question);
 
 			$html .= $question_template->get_question_html($i);
@@ -71,5 +135,23 @@ class Quizmaker {
 		$html .= "</form>";
 
 		return $html;
+	}
+
+	public static function Get_Quiz_Container($title, $quiz_html) {
+
+		$html = "<div class='container mt-2 mb-2 ml-2'>";
+
+		$html .= "<h1>" . $title . "</h1>";
+
+		$html .= "</div>";
+
+		$html .= "<div class='container mt-2'>";
+
+		$html .= $quiz_html;
+
+		$html .= "</div>";
+
+		return $html;
+
 	}
 }
--- index.php
+++ index.php
@@ -6,14 +6,6 @@ error_reporting(E_ALL);
 
 include "templates/header.php";
 include "templates/navbar.php";
-?>
-
-
-
-
-
-
-<?php
 
 include "classes/Hydrocarbons.php";
 include "classes/Alkanes.php";
@@ -39,86 +31,23 @@ if (isset($_GET['page'])) {
 
 	if ($_GET['page'] == "dynamic-quiz") {
 
-		echo "<div class='container mt-2 mb-2 ml-2'>";
+		$quiz_html = Quizmaker::Get_Dynamic_Quiz_HTML_Form(Quizmaker::Get_Sample_Questions(), "index.php");
 
-		echo "<h1>Dynamically-Generated Quiz</h1>";
-
-		echo "</div>";
-
-		echo "<div class='container mt-2'>";
-
-		$sample_quiz1 = Quizmaker::Make_Sample_Quiz1();
-
-		echo $sample_quiz1->get_quiz_html_form("index.php");
-
-		echo "</div>";
+		echo Quizmaker::Get_Quiz_Container("Dynamically-Generated Quiz", $quiz_html);
 	}
 
 	if ($_GET['page'] == "db-quiz") {
-
-		echo "<div class='container mt-2 mb-2 ml-2'>";
-
-		echo "<h1>Database-Driven Quiz</h1>";
-
-		echo "</div>";
-
-		echo "<div class='container mt-2'>";
-
 		include "inc/functions.php";
+
 		$questions = get_all_questions();
 
-		foreach ($questions as $question) {
-			$id = $question['question_number'];
-			$topic = $question['topic'];
-			$text = $question['question_text'];
-			$answer = $question['correct_answer'];
-			echo "<p><b>(" . $id . ")</b> " . $text . "<p>";
+		$quiz_html = Quizmaker::Get_DB_Quiz_Form($questions, "index.php");
 
-			echo "<select name='question" . $topic . $id . "' id='question" . $topic . $id . "'>";
+		echo Quizmaker::Get_Quiz_Container("Database-Driven Quiz", $quiz_html);
 
-			$choice_A = $question['choice_A'];
-			$choice_B = $question['choice_B'];
-			$choice_C = $question['choice_C'];
-			$choice_D = $question['choice_D'];
-
-			echo "<option value='A'>";
-			echo $choice_A;
-			echo "</option>";
-
-			echo "<option value='B'>";
-			echo $choice_B;
-			echo "</option>";
-
-			echo "<option value='C'>";
-			echo $choice_C;
-			echo "</option>";
-
-			echo "<option value='D'>";
-			echo $choice_D;
-			echo "</option>";
-
-			if (isset($question['choice_E'])) {
-				$choice_E = $question['choice_E'];
-				echo "<option value='E'>";
-				echo $choice_E;
-				echo "</option>";
-			}
-
-			echo "</select>";
-
-		}
-
-		echo "<input type='hidden' name='answer_" . $topic . $id . "' value=" . $answer . ">";
-		echo "</div>";
 	}
 }
 ?>
-
-
-
-
-
-
 
 <?php
 include "templates/footer_menu.php";
