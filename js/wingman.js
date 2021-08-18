195f10199b2a206f5e4a66cb8e78f4ed0f05516d
mac-book-review <alexmack235711@protonmail.com>
2021-08-17 19:27:57 -0600

    updated file

--- js/wingman.js
+++ js/wingman.js
@@ -7,14 +7,105 @@ class Wingman extends Sprite{
 
 			super("/assets/Wingman/wingMan1.png",
 				x,y,30,30,canvas);
-			this.velocityX = 5;
+			this.velocityX = 2;
 			this.velocityY = 0;
 			this.timer = 0;
 			this.img.style.zIndex = -10;
+
+			this.destinationX = 0;
+			this.destinationY = 0
+			this.scaleValue = 1;
+			this.scaleDelta = .1;
+			this.rotationAmount = 10;
 			
 		}
 
-		updatePhysics(timeDiff){
+		resetDestinationPoint(){
+			this.destinationY = (Math.random()*200)+40;
+			this.destinationX = (Math.random()*200) + 20;
+		}
+
+		//must have access to current texture
+		//keep trying
+		scaleMovement(timeDiff){
+
+			this.timer += timeDiff;
+			if(this.timer > 300){
+				
+				if(this.scaleValue >= 2){
+						this.scaleDelta = -.1;
+				}
+
+				if(this.scaleValue <= 0.5){
+						this.scaleDelta = .1;
+				}
+
+				//this.img.style.transform = "rotateZ(" + this.rotationAmount +"deg)";
+				//this.img.style.transform = "scale("+this.scaleValue+")";
+				this.scaleValue += this.scaleDelta;
+
+				this.timer = 0;
+			}
+
+			
+
+		}
+
+		//need to make sure has intial xVelocity
+		moveInTriangle1(timeDiff){
+
+			this.timer += timeDiff;
+			if(this.timer > 300){
+
+				if(this.velocityX > 0 && this.velocityY == 0){
+					this.velocityY = 2;
+					this.velocityX = -2;
+				} else if(this.velocityX < 0 && this.velocityY > 0){
+					this.velocityY = -2;
+					this.velocityX = -2;
+				} else if(this.velocityY < 0 && this.velocityX <0){
+					this.velocityY = 0;
+					this.velocityX = 2;
+				}
+				this.timer = 0;
+			}
+
+			this.x += this.velocityX;
+			this.y += this.velocityY;
+
+		}
+		moveTowardsDestinationPoint(timeDiff){
+
+			this.timer += timeDiff;
+			if(this.timer > 100){
+
+				//this.velocityX < 0.1 || this.velocityY < 0.1
+				//Math.abs(this.x - this.destinatioX) < 10 && Math.abs(this.y - this.destinationY) < 10
+				if(Math.abs(this.x - this.destinatioX) < 10 && Math.abs(this.y - this.destinationY) < 10){
+					console.log("Resetting destination point");
+					this.resetDestinationPoint();
+				} else {
+					console.log("Moving towards destination point");
+					var xDiff = this.destinationX - this.x;
+					var yDiff = this.destinationY - this.y;
+
+					this.velocityX = xDiff*.05;
+					this.velocityY = yDiff*.05;
+				}
+
+				this.timer = 0;
+			}
+
+			this.x += this.velocityX;
+			this.y += this.velocityY;
+			
+
+
+		}
+
+		//must have an intitial x or y velocity
+		moveInSquarePattern(timeDiff){
+
 			this.timer += timeDiff;
 			if(this.timer > 150){
 				
@@ -42,7 +133,11 @@ class Wingman extends Sprite{
 			
 			this.x += this.velocityX;
 			this.y += this.velocityY;
-			
+
+		}
+
+		updatePhysics(timeDiff){
+			this.moveInSquarePattern(timeDiff);
 
 		}
 
